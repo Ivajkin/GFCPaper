1483f36d16f33d9f532cae523655702a4cd5019b
Ivaykin Timofey <timatnet@gmail.com>
2013-10-20 11:26:10 +0000

    Update node.js

--- node.js
+++ node.js
@@ -1,8 +1,18 @@
+console.log('Init...');
+
 var net = require('net');
 
 var server = net.createServer(function (socket) {
+  console.log('server connected');
+  socket.on('end', function() {
+    console.log('server disconnected');
+  });
   socket.write('Echo server\r\n');
   socket.pipe(socket);
 });
 
-server.listen(1337, '127.0.0.1');
+server.listen(8124, function() {
+  console.log('server bound');
+});
+
+// у каждого нода - список тех, кто в сети. Когда запускаем async - передаём задачу на соседние наименее нагруженные и себе задачу оставляем тоже. Ожидаем результата в каллбеке.
